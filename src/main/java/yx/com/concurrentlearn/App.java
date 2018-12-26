187c42ca084c530fd0dc82da1e9b3ab88e848d4f
mxzenf <93839187@qq.com>
2018-12-26 10:11:18 +0800

    初始化项目

---
+++ src/main/java/yx/com/concurrentlearn/App.java
@@ -0,0 +1,13 @@
+package yx.com.concurrentlearn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
